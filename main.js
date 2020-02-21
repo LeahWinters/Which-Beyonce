fd7cb0ac2ca04199d5e12dc64d4d080cd56f74fb
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-02-20 21:05:07 -0700

    Functionality for clicking on a card

--- main.js
+++ main.js
@@ -1,33 +1,36 @@
-var cards = []
-var deck = new Deck(cards);
+var deck = new Deck();
+var cardPictures = ['./assets/noGodPleaseNoo.jpg', './assets/prisonMike.jpg', './assets/sexySanta.jpg', './assets/tellMeMore.jpg', './assets/worldsBestBoss.jpg'];
 
 window.addEventListener('load', displayCards);
 
-
-function pushToDeck() {
-  console.log(deck)
-  for (var i = 0; i < 10 ; i++) {
-    var newCard = new Card('mike' + i, './assets/image' + i + '.jpg')
-    cards.push(newCard)
-  }
-}
-pushToDeck() //invoking function from above
-
-
-
 function displayCards() {
-  var allCards = deck.cards.length;
+  var allCards = deck.cards;
   var cardHolderSection = document.querySelector('.container-to-all-cards');
   console.log(allCards)
-  for (var i = 0; i < cards.length; i++) {
+  for (var i = 0; i < 10; i++) {
     cardHolderSection.insertAdjacentHTML('afterend',
-    `<section class="card-container">
+      `<section onClick="clickedCard(${i})" class="card-container">
     <div class="card" id=${"card" + i}></div>
     </section>`);
-    // ${cards[i].cardPhoto}
   }
 }
 
-function clickCard() {
+function clickedCard(i) {
+  if (deck.selectedCards.length < 2) {
+    changePicture(i);
+    deck.selectedCards.push(deck.cards[i]);
+  } else {
+    console.log("Too many cards")
+  }
+}
 
+function changePicture(i) {
+  var individualCards = document.querySelectorAll('.card-container');
+  var card = document.querySelectorAll('.card');
+  // console.log(i, individualCards[i])
+  var myArray = Array.from(individualCards);
+  var reversedArray = myArray.reverse()
+  if (i === 0 || i === 1) {
+    reversedArray[i].children[0].style.backgroundImage = "url('./assets/noGodPleaseNooo.jpg')";
+  }
 }
